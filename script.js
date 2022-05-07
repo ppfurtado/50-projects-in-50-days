d859d221f35bc77bdc9d2e85843074fa02d35168
Pedro Furtado <pedro.nazare@pdcase.com.br>
2022-05-07 13:19:56 -0300

    Add: create method for add class active for next image

--- script.js
+++ script.js
@@ -5,13 +5,11 @@ panels.forEach((panel) => {
     panel.addEventListener('click', () => {
         removeActiveClass();
         panel.classList.add('active');
-        panel.style.backgroundSize = "100% 100%"
     })
 })
 
 function removeActiveClass(){
     panels.forEach((panel) => {
-        panel.style.backgroundSize = "auto 100%"
         panel.classList.remove('active');
     })
 }
